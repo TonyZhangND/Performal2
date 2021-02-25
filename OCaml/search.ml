02c715104685b3967baba151822b8255f1ae5104
Tony Zhang <nz97@cornell.edu>
2021-02-25 11:28:15 -0500

    refactor dfs function

--- OCaml/search.ml
+++ OCaml/search.ml
@@ -2,6 +2,25 @@ open Toylock
 
 type run_tree = RNode of system * run_tree list
 
+
+(* requires: ids is a complete list of hosts in curr_sys *)
+(* returns: a list of possible next states of curr_sys *)
+let rec gen_child_syslist (curr_sys:system) (ids:address list) (acc:system list) : system list =
+    
+    match ids with 
+    | [] -> acc
+    | h :: t -> 
+        let acc = 
+            if grant_enabled curr_sys h then (node_grant curr_sys h)::acc else acc
+        in
+        let acc = 
+            if accept_enabled curr_sys h then (node_accept curr_sys h)::acc else acc
+        in
+        gen_child_syslist curr_sys t acc
+
+
+(* requires: limit_depth > 0, curr_depth >= 0, limit_depth >= curr_depth *)
+(* returns: an execution tree with initial state root and depth up to limit_depth *)
 let rec dfs (root: run_tree) (curr_depth:int) (limit_depth:int) : run_tree =
     (* Printf.printf "dfs depth %d \n" curr_depth; *)
     let sys = match root with |RNode(sys, c) -> sys in 
@@ -10,33 +29,19 @@ let rec dfs (root: run_tree) (curr_depth:int) (limit_depth:int) : run_tree =
     else
     (* Recursive case *)
     (* First compute the children list of root *)
-    
-    let rec gen_child_syslist (curr_sys:system) (ids:address list) (acc:system list) : system list =
-        (* Given the current system state, return a list of possible next states *)
-        match ids with 
-        | [] -> acc
-        | h :: t -> 
-            let acc = 
-                if grant_enabled curr_sys h then (node_grant curr_sys h)::acc else acc
-            in
-            let acc = 
-                if accept_enabled curr_sys h then (node_accept curr_sys h)::acc else acc
-            in
-            gen_child_syslist curr_sys t acc
-    in
-    let rec gen_subtrees (syslist:system list) (acc:run_tree list) : run_tree list =
+    (* Return node with value sys, and children are the [dfs(c)] of each c in children *)
+    let child_syslist = gen_child_syslist sys sys.config []  in
+    gen_subtrees sys child_syslist curr_depth limit_depth []
+    and gen_subtrees (parent:system) (syslist:system list) (curr_depth:int) (limit_depth:int) (acc:run_tree list) 
+        : run_tree =
         (* Given a list of possible next states as syslist, return a list of run_tree 
          * that represents the list of children/subtrees of root 
          *)
         match syslist with
-        | [] -> acc 
+        | [] -> RNode(parent, acc)
         | h :: t -> 
             let subtree = dfs (RNode(h, [])) (curr_depth + 1) limit_depth in
-            gen_subtrees t (subtree::acc)
-    in
-    (* Return node with value sys, and children are the [dfs(c)] of each c in children *)
-    let child_syslist = gen_child_syslist sys sys.config []  in
-    RNode(sys, (gen_subtrees child_syslist []))
+            gen_subtrees parent t curr_depth limit_depth (subtree::acc)
         
 
 (* requires: n>0, limit_depth>0 *)
@@ -63,7 +68,7 @@ let rec print_tree (t:run_tree) : unit =
 
 let _ = 
     print_endline "Toylock run generator";
-    let tree = search 3 10 in
+    let tree = search 3 100_000 in   (* Goes into stack overflow at 200_000 *)
     let _ = print_tree tree in
     print_endline "Done";
     ()
